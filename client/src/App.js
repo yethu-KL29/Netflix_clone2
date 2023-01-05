c1edafb8821d46e39691498f770763f930c073db
Yethu <yethu@example.com>
2023-01-05 20:23:49 +0530

    unwanted codes rmoved and navbar session added

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,19 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { Navbar } from "./components/nav/Navbar";
 
 function App() {
-  
+   return(
+    <div className="app">
+     <BrowserRouter>
+      <Routes>
+       <Route path="/" element={<Navbar/>}/>
+
+      </Routes>
+     
+     </BrowserRouter>
+
+    </div>
+   )
 }
 
 export default App;
--- client/src/components/nav/Navbar.js
+++ client/src/components/nav/Navbar.js
@@ -1,62 +1,62 @@
 import React from 'react'
 import { Button, Input, TextField, Typography, InputLabel, Tab, Tabs, Toolbar, AppBar, IconButton } from '@mui/material'
-import "./style.css"
-import Searchbar from './Searchbar'
-import { useState } from 'react'
-import Logo from "../components/Logo.png"
+import Logo from "../nav/Logo.png"
 import PowerSettingsNewIcon from '@mui/icons-material/PowerSettingsNew';
 import { Link, useNavigate } from 'react-router-dom'
+import { useState } from 'react';
+import "./Navbar.css"
 
 export const Navbar = () => {
+    const [value, setvalue] = useState()
     return (
-        <div className='netflix'>
-            <HomeImage></HomeImage>
-            <div className={color ? 'heading-bg' : 'heading'}>
-                <div className='nav'>
-                    <div className='logo'>
-                        <img src={Logo}></img>
-                    </div>
+        <div className='Navbar'>
+            <div className='content'>
+                <div className='logo'>
+                    {/* <img src={Logo}></img> */}
+                </div>
+                <div>
                     <Toolbar sx={{
-                        width: "100%"
+                        width: "100vw"
                     }}>
                         <Tabs TabIndicatorProps={{ style: { background: 'red' } }} value={value} onChange={(e, val) => {
                             setvalue(val)
                         }} sx={{ backgroundColor: "inherit" }}>
                             <Tab sx={{
                                 color: "white",
-                                fontSize: '1.5vh',
+                               
                             }} label="Home" />
 
                             <Tab LinkComponent={Link} to="/series" sx={{
                                 color: "white",
-                                fontSize: '1.5vh',
+                                
                             }} label="series" />
 
                             <Tab LinkComponent={Link} to="/movies" sx={{
                                 color: "white",
-                                fontSize: '1.5vh'
+                               
                             }} label="Movies" />
 
                             <Tab sx={{
                                 color: "white",
-                                fontSize: '1.5vh'
+                               
                             }} label="popular" />
                             <Tab sx={{
                                 color: "white",
-                                fontSize: '1.5vh',
+                               
                             }} label="My List" />
 
                         </Tabs>
                     </Toolbar>
+                </div>
 
-                </div>
-                <div className='logout'>
-                    <Searchbar></Searchbar>
-                    <IconButton onClick={handleLogout} sx={{ color: 'red', width: '3vw' }}>
-                        <PowerSettingsNewIcon />
-                    </IconButton>
-                </div>
             </div>
+            <div className='logout'>
+
+                <IconButton sx={{ color: 'red', width: '3vw' }}>
+                    <PowerSettingsNewIcon />
+                </IconButton>
+            </div>
+
 
         </div>
     )
