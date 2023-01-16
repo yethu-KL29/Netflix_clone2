00533c8f71bc75a9b03e6d0795d4bcd7f438cb3f
Yethu <Yethukichu55@gmail.com>
2023-01-16 21:01:40 +0530

    app.js updated

---
+++ server/app.js
@@ -0,0 +1,40 @@
+import {BrowserRouter,Routes,Route,Redirect } from 'react-router-dom';
+import Login from './pages/Login';
+import Signup from './pages/Signup';
+import Netflix from './pages/Netflix';
+import "../src/index.css"
+import Player from './pages/Player';
+import Main from './pages/Main';
+function App() {
+
+  const user=true;
+  return (
+    <div className="App">
+    <BrowserRouter>
+      <Routes>
+       {!user && <Route path="/" element={<Signup/>}/>}
+       <Route path="/login" element={<Login/>}/>
+        { user && 
+        <>
+         <Route path="/" element={<Main/>}/>
+
+      
+
+        <Route path="/movies" element={<Main type="movies"/>}/>
+
+        <Route path="/series" element={<Main type="series"/>}/>
+
+      
+
+        <Route path="/player" element={<Player/>}/>
+        </>
+       } 
+      </Routes>
+    </BrowserRouter> 
+    
+    
+   </div>
+  );
+}
+
+export default App;
