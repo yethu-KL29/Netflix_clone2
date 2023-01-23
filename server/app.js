791f01576d353c791abec2bc8636b88aa63dd6de
Yethu <Yethukichu55@gmail.com>
2023-01-23 20:16:15 +0530

    updated

--- server/app.js
+++ server/app.js
@@ -10,28 +10,8 @@ function App() {
   const user=true;
   return (
     <div className="App">
-    <BrowserRouter>
-      <Routes>
-       {!user && <Route path="/" element={<Signup/>}/>}
-       <Route path="/login" element={<Login/>}/>
-        { user && 
-        <>
-         <Route path="/" element={<Main/>}/>
-
-      
-
-        <Route path="/movies" element={<Main type="movies"/>}/>
-
-        <Route path="/series" element={<Main type="series"/>}/>
-
-      
-
-        <Route path="/player" element={<Player/>}/>
-        </>
-       } 
-      </Routes>
-    </BrowserRouter> 
     
+     
     
    </div>
   );
