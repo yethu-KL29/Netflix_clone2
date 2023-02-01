995a5ae936eddcabd3f3fa7345739a711e73af59
Yethu <Yethukichu55@gmail.com>
2023-02-01 19:55:45 +0530

    navbar modified and make it response

--- client/src/components/nav/Navbar.js
+++ client/src/components/nav/Navbar.js
@@ -3,39 +3,34 @@ import { Tab, Tabs, Toolbar, AppBar, IconButton } from '@mui/material'
 import Logo from "../nav/Logo.png"
 import PowerSettingsNewIcon from '@mui/icons-material/PowerSettingsNew';
 import { useState } from 'react';
+import './Navbar.css'
 
 export const Navbar = () => {
-    const [value, setvalue] = useState()
+
     return (
         <div className='Navbar'>
-            <div className='left-content'>
-                <div className='logo'>
-                    <img src={Logo}></img>
-                </div>
-                <div>
-                   
-                        <Tabs>
-                        
-                       
-                       
+            <div className='container'>
+                <div className='container-left'>
+                    <img src={Logo} alt="Logo" />   
+                     <Tabs  TabIndicatorProps={{ sx: { display: 'none' } }}
+                     sx={{
+                        '& .MuiTabs-flexContainer': {
+                          flexWrap: 'wrap',
+                        },
+                      }}
                      
-                        
-                        <Tab label="Home" sx={{color:"white",width: '3vw'}} />
-                   
-                        
-                       
+                    >
+                        <Tab sx={{ color:'white'}} label="HomePage" />
+                        <Tab sx={{ color:'white'}} label="About" />
+                        <Tab sx={{ color:'white'}} label="Contact" />
+                        <Tab sx={{ color:'white'}} label="Login" />
+                        <Tab sx={{ color:'white'}} label="Register" />
 
-                     </Tabs>
-                    
+                    </Tabs>
+                
                 </div>
-
-            </div>
-            <div className='right-content'>
-
-                <IconButton sx={{ color: 'red', width: '3vw' }}>
-                    <PowerSettingsNewIcon />
-                </IconButton>
-              
+                
+                   
             </div>
 
 
